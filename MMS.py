83d82f3c5a62ba31f1314a597606dd3439929c0f
FireCoder29 <arhammd887@gmail.com>
2026-01-31 17:23:58 +0600

    Added child class & function

--- MMS.py
+++ MMS.py
@@ -52,3 +52,47 @@ class Parent(Person):
             for category, amount in self.expenses.items():
                 print(f" - {category}: {amount} Taka")
         print(f"Remaining Balance: {self.remaining_balance} Taka")
+
+    
+    class Child(Person):
+        def __init__(self, name):
+            super().__init__(name, role="Child")
+            self.pocket_money = 0
+            self.spent = 0
+            self.total_received = 0
+
+        def receive_money(self, amount):
+            if amount <= 0:
+                print("Invalid Amount! Received amount must be greater than zero.")
+                return
+            
+            self.pocket_money += amount
+            self.total_received += amount
+            print(f"{self.name} received {amount} Taka as pocket money.")
+
+        def receive_extra_income(self, amount):
+            if amount <= 0:
+                print("Invalid Amount! Extra income must be greater than zero.")
+                return
+
+            self.pocket_money += amount
+            self.total_received += amount
+            print(f"{self.name} received {amount} Taka as extra income.")
+
+        def spend_money(self, amount):
+            if amount <= 0:
+                print("Invalid Amount! Spend amount must be greater than zero.")
+                return
+
+            if amount > self.pocket_money:
+                print("Not enough pocket money to spend")
+            else:
+                self.pocket_money -= amount
+                self.spent += amount
+                print(f"{self.name} spent {amount} Taka.")
+
+        def show_balance(self):
+            print(f"{self.name}'s Pocket Money Details:")
+            print(f" - Total Received: {self.total_received} Taka")
+            print(f" - Total Spent: {self.spent} Taka")
+            print(f" - Remaining Pocket Money: {self.pocket_money} Taka") 
